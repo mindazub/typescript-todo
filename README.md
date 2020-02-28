96ccf0af0c7d51116bede0f75066f96e7cdecfc8
Mindaugas Ažubalis <mindaugas.azubalis@gmail.com>
2020-02-28 17:07:39 +0200

    added Readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+#source https://levelup.gitconnected.com/react-typescript-with-webpack-part-2-aa02d150c59b
+
+#video https://www.youtube.com/playlist?list=PLiKs97d-BatHEeclprFtCaw8RcNOYXUqN
+
+Start the project: npm start
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,21 +14,52 @@ export class App extends React.Component<{}, IState> {
             currentTask: "",
             tasks: [
                 ...this.state.tasks,
-                this.state.currentTask
+                {
+                    id: this._timeInMilliseconds(),
+                    value: this.state.currentTask,
+                    completed: false
+                }
             ]
-        })
+        });
+    }
 
+    public deleteTask(id: number): void {
+        const filteredTasks: Array<ITask> = this.state.tasks.filter(
+            (task: ITask) => task.id !== id);
+        this.setState({ tasks: filteredTasks });
+    }
+
+    public toggleDone(index: number): void {
+        let task: ITask[] = this.state.tasks.splice(index, 1);
+        // cia nulis todel kad kai issirenki task is visu ir tada jo index vel tampa 0
+        task[0].completed = !task[0].completed;
+        // debugger;
+        const currentTasks: ITask[] = [...this.state.tasks, ...task];
+        // galima palikti ir taip, kad tik visi vardai butu tokie patys
+        this.setState({ tasks: currentTasks });
     }
 
     public renderTasks(): JSX.Element[] {
-        return this.state.tasks.map((task: string, index: number) => {
-            return (<>
-                
-                    <div key={index}>{task}</div>
-                
-            </>)
+        return this.state.tasks.map((task: ITask, index: number) => {
+            return (
+            
+
+                <div key={task.id} className="tdl-task">
+
+                    <span className={task.completed ? 'is-completed' : ''}>{task.value}</span>
+
+                    <button onClick={() => this.deleteTask(task.id)}>Delete</button>
+                    <button onClick={() => this.toggleDone(index)}>
+                        { task.completed ? 'Undo' : 'Done' }
+                    </button>
+
+                </div>
+
+            )
         });
     }
+
+
 
     public render(): JSX.Element {
 
@@ -38,9 +69,13 @@ export class App extends React.Component<{}, IState> {
             <>
                 <h1>React Typescript Todo List</h1>
                 <form onSubmit={(e) => this.handleSubmit(e)}>
-                    <input type="text" placeholder="Add a Task"
+                    <input
+                        type="text"
+                        className="tdl-input"
+                        placeholder="Add a Task"
                         value={this.state.currentTask}
-                        onChange={(e) => this.setState({ currentTask: e.target.value })} />
+                        onChange={(e) => this.setState({ currentTask: e.target.value })}
+                    />
                     <button type="submit">Add Task</button>
                 </form>
                 <section>
@@ -48,10 +83,22 @@ export class App extends React.Component<{}, IState> {
                 </section>
             </>);
     }
+
+    private _timeInMilliseconds(): number {
+        const date: Date = new Date();
+        return date.getTime();
+    }
+
 }
 
 
 interface IState {
     currentTask: string,
-    tasks: Array<string>
+    tasks: Array<ITask>
+}
+
+interface ITask {
+    id: number;
+    value: string;
+    completed: boolean
 }
